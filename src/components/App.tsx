f9617580c380e68eafe0f28b111ae2623ea32b4a
Maxime Galliot <maximegalliot@hotmail.fr>
2023-11-13 11:03:53 +0100

    File structure

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
-import reactLogo from './assets/stylesheets/images/react.svg'
+import reactLogo from '../assets/stylesheets/images/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import '../assets/stylesheets/components/App.css'
 
 function App() {
   const [count, setCount] = useState(0)
