9971ebd3dd43639ee3cfa43b9c4b27151df2cec3
leo <peirolo@yahoo.fr>
2023-11-20 09:47:44 +0100

    menu done need burger button

--- src/SideBar.tsx
+++ src/SideBar.tsx
@@ -12,6 +12,7 @@ transition pour déploiement du menu
 
 import { useState } from 'react';
 import Button from './Button';
+import './SideBar.css'
 // import { useState } from "react";
 
 
@@ -20,37 +21,51 @@ const SideBar = () => {
     const [isClicked, setIsClicked] = useState(false);
     const [classButton, setClassButton] = useState("button visible")
     const [classMenu, setClassMenu] = useState("menu hidden")
+    const [classSideBar, setSideBar] = useState("sidebar")
     
 
 
    
     const updateMenu = () => {
         if(!isClicked) {
-            setClassButton("burger-bar clicked")
+            setClassButton("burger-bar clicked button")
             setClassMenu("menu visible")
+            setSideBar("classSideBar visible classMenu")
         }
         else {
-            setClassButton("burger-bar unclicked")
+            setClassButton("burger-bar unclicked button")
             setClassMenu("menu hidden")
+            setSideBar("classSideBar")
         }
         setIsClicked(!isClicked)
     }
 
-    // if(!isClicked){
+
         return(
             <div>
-                <nav>
+                <nav className={classSideBar}>
                     <div className={classButton}>
                     <Button isClicked={isClicked} onClick={updateMenu} label={"button"}/>
                     </div>
-                </nav>
 
-                <div className={classMenu}>
-                    <h1>menu</h1>
-                </div>
+                    <div className={classMenu}>
+                        
+                        <a className='itemMenu item1' href='/'>item1</a>
+                        
+                        <a className='itemMenu' href='/'>item2</a>
+                        
+                        <a className='itemMenu' href='/'>item3</a>
+
+                        <a className='itemMenu' href='/'>item4</a>
+
+                        <a className='itemMenu' href='/'>item5</a>
+
+                        <a className='itemMenu' href='/'>item6</a>
+
+                    </div>
+                </nav>
             </div>
         );
-    // }
 
 }
 
