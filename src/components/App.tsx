567355fade841fc53e03d8b71273a3cc902c5250
Kevin44France <kevin.sen@themama.ai>
2023-12-11 11:43:54 +0100

    Formulaire done, juste lottie animation must be optimized

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,7 @@
-import React, { useState } from 'react';
-import { Helmet } from 'react-helmet';
+import React, {useState} from 'react';
 import '../assets/stylesheets/components/App.css';
 import TaskForm from './taskForm/TaskForm';
-import BlueButton from './buttons/BlueButton';
+import Tasks from "./tasks/Tasks.tsx";
 
 function App(): JSX.Element {
     const [tasks, setTasks] = useState<string[]>([]);
@@ -11,31 +10,11 @@ function App(): JSX.Element {
         setTasks([...tasks, task]);
     };
 
-    const deleteTask = (index: number) => {
-        const newTasks = [...tasks];
-        newTasks.splice(index, 1);
-        setTasks(newTasks);
-    };
-
     return (
         <div className="App">
-            <Helmet>
-                <title>Your Page Title</title>
-                <script src="https://unpkg.com/@dotlottie/player-component@latest/dist/dotlottie-player.mjs" type="module"></script>
-            </Helmet>
             <h1>React To-Do App</h1>
-            <BlueButton onClick={() => setTasks([])}>Clear All</BlueButton>
-            <TaskForm onAddTask={addTask} />
-            <ul>
-                {tasks.map((task, index) => (
-                    <li key={index}>
-                        {task.name} - Category: {task.category} - Date: {task.date} - Collaborators: {task.collaborators}
-                        <BlueButton onClick={() => deleteTask(index)}>Delete</BlueButton>
-                    </li>
-                ))}
-            </ul>
-
-            <dotlottie-player src="https://lottie.host/6b93de08-74fd-4deb-91ff-29f270eb0708/oa3NRa2ClI.json" background="transparent" speed="1" style={{ width: '300px', height: '300px' }} loop autoplay></dotlottie-player>
+            <TaskForm onAddTask={addTask}/>
+            <Tasks/>
         </div>
     );
 }
--- src/components/taskForm/TaskForm.tsx
+++ src/components/taskForm/TaskForm.tsx
@@ -1,81 +1,114 @@
-import React, { useState } from 'react';
-import Lottie from 'react-lottie';
-import fadeInAnimation from './Animation - 1700475415786.json'
-interface TaskFormProps {
-    onAddTask: (task: Task) => void;
-}
+import React, {useState} from 'react';
+import Lottie from 'lottie-react';
+import animationData from './animrouge.json'
+import '../../assets/stylesheets/components/taskForm.css';
+import BlueButton from "../buttons/blueButton";
+import DatePicker from 'react-datepicker';
+import "react-datepicker/dist/react-datepicker.css";
+import firebase from "../../firebase/firebase.tsx";
+
 
 interface Task {
-    id: number;
     name: string;
     category: string;
-    date: string;
+    date: Date;
     collaborators: string;
 }
 
-const TaskForm: React.FC<TaskFormProps> = ({ onAddTask }) => {
+const TaskForm: React.FC = () => {
     const [newTask, setNewTask] = useState<Task>({
-        id: 0,
         name: '',
         category: '',
-        date: '',
+        date: new Date(),
         collaborators: '',
     });
-
-    const [animationVisible, setAnimationVisible] = useState(false);
+    const [isAnimating, setIsAnimating] = useState(false);
 
     const handleInputChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
-        const { name, value } = e.target;
+        const {name, value} = e.target;
         setNewTask({
             ...newTask,
             [name]: value,
         });
     };
+    const handleDateChange = (date: Date) => {
+        setNewTask({
+            ...newTask,
+            date,
+        });
+    };
 
     const handleAddTask = () => {
         if (newTask.name.trim() !== '') {
-            onAddTask(newTask);
-            setNewTask({
-                id: 0,
-                name: '',
-                category: '',
-                date: '',
-                collaborators: '',
-            });
+            const db = firebase.firestore();
 
+            // Vérifiez que les champs select ne sont pas vides
+            if (newTask.category !== '' && newTask.collaborators !== '') {
+                db.collection("tasks").add(newTask);
 
-            // Set the state to make the animation visible
-            setAnimationVisible(true);
-
-            // Set autoplay to false after the animation is played
-            setTimeout(() => {
-                setAnimationVisible(false);
-            }, 500); // Adjust the duration based on your animation
+                // Animation and state reset logic
+                setIsAnimating(true);
+                setTimeout(() => {
+                    setIsAnimating(false);
+                    setNewTask({
+                        name: '',
+                        category: '',
+                        date: new Date(),
+                        collaborators: '',
+                    });
+                }, 3000);
+            } else {
+                // Affichez un message d'erreur ou effectuez une action appropriée pour indiquer à l'utilisateur de remplir les champs select.
+                alert('Please select options for Category and Collaborators');
+            }
         }
     };
 
-    const fadeInOptions = {
-        loop: false,
-        autoplay: animationVisible, // Only autoplay when the button is clicked
-        animationData: fadeInAnimation,
-        rendererSettings: {
-            preserveAspectRatio: 'xMidYMid slice',
-        },
-    };
 
     return (
-        <div>
-            {/* Your existing form elements */}
+        <div className="task-form-container">
             <label>
                 Task Name:
-                <input type="text" name="name" value={newTask.name} onChange={handleInputChange} />
+                <input type="text" name="name" value={newTask.name} onChange={handleInputChange}/>
             </label>
-            {/* ... (other form elements) */}
+            <label>
+                Date:
+                <DatePicker
+                    selected={newTask.date}
+                    onChange={handleDateChange}
+                    showTimeSelect
+                    timeFormat="HH:mm"
+                    timeIntervals={15}
+                    timeCaption="Heure"
+                    dateFormat="MMMM d, yyyy h:mm aa"
+                    wrapperClassName="datePicker"
+                />
+            </label>
+            <label>
+                Category:
+                <select name="category" value={newTask.category} onChange={handleInputChange}>
+                    <option value="" disabled>Select a category</option>
+                    <option value="Food">Food</option>
+                    <option value="Work">Work</option>
+                    <option value="Sports">Sports</option>
+                </select>
+            </label>
+            <label>
+                Collaborators:
+                <select name="collaborators" value={newTask.collaborators} onChange={handleInputChange}>
+                    <option value="" disabled>Select a collaborator</option>
+                    <option value="Kevin">Kevin</option>
+                    <option value="Daykwaza">Daykwaza</option>
+                </select>
+            </label>
 
-            {/* Add the Lottie animation for fade-in */}
-            <Lottie options={fadeInOptions} height={50} width={50} />
 
-            <button onClick={handleAddTask}>Add</button>
+            <div className="button-and-animation-container">
+                <BlueButton onClick={handleAddTask}>Add Task</BlueButton>
+                {isAnimating && (
+                    <Lottie animationData={animationData} loop={false} style={{width: '50px', height: '50px'}}/>
+                )}
+            </div>
         </div>
     );
 };
