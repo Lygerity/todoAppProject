1e0a1dd65ad22bef6980770654b310fb91467c43
Maxime Galliot <maximegalliot@hotmail.fr>
2024-01-15 08:34:09 +0100

    :sparkles: Edit button

--- src/components/tasks/task/Task.tsx
+++ src/components/tasks/task/Task.tsx
@@ -1,7 +1,8 @@
-import {FunctionComponent, PropsWithChildren, useState} from "react";
-import {TaskAlt, RemoveDone} from '@mui/icons-material';
+import {PropsWithChildren, useState} from "react";
+import {TaskAlt, RemoveDone, Edit} from '@mui/icons-material';
 import "../../../assets/stylesheets/components/Task.css"
 import firebase from "../../../firebase/firebase.tsx";
+import BlueButton from "../../buttons/blueButton.tsx";
 
 type Props = {
     id: string;
@@ -10,9 +11,11 @@ type Props = {
     completed: boolean;
 } & PropsWithChildren;
 
-const Task:FunctionComponent<Props> = (props: Props) => {
-
-    const [status, setStatus] =useState(props.completed)
+const Task = (props: Props) => {
+    const [status, setStatus] = useState(props.completed);
+    const [isEditing, setIsEditing] = useState(false);
+    const [editedName, setEditedName] = useState(props.name);
+    const [editedDate, setEditedDate] = useState(props.date);
 
     const handleClick = () =>{
         const tasksRef = firebase.firestore().doc('tasks/'+props.id);
@@ -21,19 +24,53 @@ const Task:FunctionComponent<Props> = (props: Props) => {
         }).then(() => setStatus(!status))
     }
 
-    const minutes:string | number = props.date.getMinutes()<10 ? "0"+props.date.getMinutes() : props.date.getMinutes()
+    const handleEditClick = () => {
+        setIsEditing(true);
+    }
 
-    return(
+    const handleSaveClick = () => {
+        const tasksRef = firebase.firestore().doc('tasks/' + props.id);
+        tasksRef.update({
+            name: editedName,
+            date: editedDate
+        }).then(() => {
+            setIsEditing(false);
+        });
+    }
+
+    return (
         <div className={"task"}>
-            <h3 className={"task__name"}>{props.name}</h3>
-            <div className={"task__date"}>
-                <p>{props.date.getDate()}/{props.date.getMonth()+1}/{props.date.getFullYear()}</p>
-                <p>{props.date.getHours()}h{minutes}</p>
+
+            <div className={"task__edit"} onClick={handleEditClick}>
+                <Edit style={{color: '#0077B6', cursor: 'pointer'}} />
             </div>
-            <div className={"task__completed"} onClick={() => handleClick()}>
-                {status ? <TaskAlt style={{color: 'lightgreen'}}/> : <RemoveDone style={{color: 'indianred'}}/>}
-            </div>
+            {isEditing ? (
+                <div>
+                    <input
+                        type="text"
+                        value={editedName}
+                        onChange={(e) => setEditedName(e.target.value)}
+                    />
+                    <input
+                        type="date"
+                        value={editedDate.toISOString().split('T')[0]}
+                        onChange={(e) => setEditedDate(new Date(e.target.value))}
+                    />
+
+                    <div className={"task__save"}>
+                        <BlueButton onClick={handleSaveClick}>Save</BlueButton>
+                    </div>
+                </div>
+            ) : (
+                <div>
+                    <h3 className={"task__name"}>{props.name}</h3>
+                    <p>{editedDate.getDate()}/{editedDate.getMonth() + 1}/{editedDate.getFullYear()}</p>
+                    <div className={"task__completed"} onClick={() => handleClick()}>
+                        {status ? <TaskAlt style={{color: 'lightgreen'}}/> : <RemoveDone style={{color: 'indianred'}}/>}
+                    </div>
+                </div>
+            )}
         </div>
-    )
+    );
 }
 export default Task
